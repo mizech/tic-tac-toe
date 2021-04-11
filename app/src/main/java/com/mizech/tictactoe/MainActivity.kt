64ad638857d355b1cc64be36de12cdea889f43d2
michael.zech <michael.zech@hotmail.de>
2021-04-11 11:29:19 +0200

    Message gets Player color.

--- app/src/main/java/com/mizech/tictactoe/MainActivity.kt
+++ app/src/main/java/com/mizech/tictactoe/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mizech.tictactoe
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,12 +8,11 @@ import android.widget.ImageView
 import com.mizech.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val TAG = this::class.java.name
     private lateinit var binding: ActivityMainBinding
 
-    var gameState = mutableListOf<FeasibleState>()
-    var isPlayerOne = true
-    val imageViews = mutableListOf<ImageView>()
+    private var gameState = mutableListOf<FeasibleState>()
+    private var isPlayerOne = true
+    private val imageViews = mutableListOf<ImageView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             gameState.add(FeasibleState.NOT_SET)
             i++
         }
-
+        // Add the imageViews to the List
         imageViews.add(binding.imageView0)
         imageViews.add(binding.imageView1)
         imageViews.add(binding.imageView2)
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         imageViews.add(binding.imageView6)
         imageViews.add(binding.imageView7)
         imageViews.add(binding.imageView8)
-
+        // Iterate the list and attach an Listener to each.
         imageViews.forEach {
             it.setOnClickListener {
                 processStateChange(it)
@@ -52,55 +52,65 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun checkGameState(currentPlayer: FeasibleState): Boolean {
-        if (gameState.get(0) === currentPlayer && gameState.get(1) === currentPlayer && gameState.get(2) === currentPlayer) {
+    private fun checkGameState(currentPlayer: FeasibleState): Boolean {
+        if (gameState[0] === currentPlayer && gameState[1] === currentPlayer
+                && gameState[2] === currentPlayer) {
             return true
         }
 
-        if (gameState.get(3) === currentPlayer && gameState.get(4) === currentPlayer && gameState.get(5) === currentPlayer) {
+        if (gameState[3] === currentPlayer && gameState[4] === currentPlayer
+                && gameState[5] === currentPlayer) {
             return true
         }
 
-        if (gameState.get(6) === currentPlayer && gameState.get(7) === currentPlayer && gameState.get(8) === currentPlayer) {
+        if (gameState[6] === currentPlayer && gameState[7] === currentPlayer
+                && gameState[8] === currentPlayer) {
             return true
         }
 
-        if (gameState.get(0) === currentPlayer && gameState.get(3) === currentPlayer && gameState.get(6) === currentPlayer) {
+        if (gameState[0] === currentPlayer && gameState[3] === currentPlayer
+                && gameState[6] === currentPlayer) {
             return true
         }
 
-        if (gameState.get(1) === currentPlayer && gameState.get(4) === currentPlayer && gameState.get(7) === currentPlayer) {
+        if (gameState[1] === currentPlayer && gameState[4] === currentPlayer
+                && gameState[7] === currentPlayer) {
             return true
         }
 
-        if (gameState.get(2) === currentPlayer && gameState.get(5) === currentPlayer && gameState.get(8) === currentPlayer) {
+        if (gameState[2] === currentPlayer && gameState[5] === currentPlayer
+                && gameState[8] === currentPlayer) {
             return true
         }
 
-        if (gameState.get(0) === currentPlayer && gameState.get(4) === currentPlayer && gameState.get(8) === currentPlayer) {
+        if (gameState[0] === currentPlayer && gameState[4] === currentPlayer
+                && gameState[8] === currentPlayer) {
             return true
         }
 
-        if (gameState.get(5) === currentPlayer && gameState.get(4) === currentPlayer && gameState.get(2) === currentPlayer) {
+        if (gameState[5] === currentPlayer && gameState[4] === currentPlayer
+                && gameState[2] === currentPlayer) {
             return true
         }
 
         return false
     }
 
-    fun processStateChange(it: View) {
+    private fun processStateChange(it: View) {
         val imgView = it as ImageView
         if (isPlayerOne) {
             imgView.setImageResource(R.drawable.player_one)
-            gameState.set(imgView.tag.toString().toInt(), FeasibleState.PLAYER_ONE)
-            if (checkGameState(FeasibleState.PLAYER_ONE) == true) {
-                binding.currentMessage.text = "Player One has won"
+            gameState[imgView.tag.toString().toInt()] = FeasibleState.PLAYER_ONE
+            if (checkGameState(FeasibleState.PLAYER_ONE)) {
+                binding.currentMessage.text = getString(R.string.one_won_message)
+                binding.currentMessage.setTextColor(Color.parseColor("#64FF64"))
             }
         } else {
             imgView.setImageResource(R.drawable.player_two)
-            gameState.set(imgView.tag.toString().toInt(), FeasibleState.PLAYER_TWO)
-            if (checkGameState(FeasibleState.PLAYER_TWO) == true) {
-                binding.currentMessage.text = "Player Two has won"
+            gameState[imgView.tag.toString().toInt()] = FeasibleState.PLAYER_TWO
+            if (checkGameState(FeasibleState.PLAYER_TWO)) {
+                binding.currentMessage.text = getString(R.string.two_one_message)
+                binding.currentMessage.setTextColor(Color.parseColor("#FF6464"))
             }
         }
 
